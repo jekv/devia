1b86837bf3a691410c0e72f908d1993973cecffc
jocampo <jomaro@globalteleforce.com>
2015-06-17 10:58:52 +0800

    db connection

--- wp-config.php
+++ wp-config.php
@@ -14,15 +14,15 @@
  * @package WordPress
  */
 
-// ** MySQL settings - You can get this info from your web host ** //
+// ** MySQL settings - You can get this info from your web host ** // 
 /** The name of the database for WordPress */
-define('DB_NAME', 'try');
+define('DB_NAME', 'devia');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'jake');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'abcdefghijklmn123');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
