4733c17928064eecf1a4d1ffcf90293192af8b64
jocampo <jomaro@globalteleforce.com>
2015-06-19 11:02:08 +0800

    Contact Form Error Check

--- wp-config.php
+++ wp-config.php
@@ -19,10 +19,10 @@
 define('DB_NAME', 'devia');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'jake');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'rootmysql@15');
+define('DB_PASSWORD', 'abcdefghijklmn123');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
