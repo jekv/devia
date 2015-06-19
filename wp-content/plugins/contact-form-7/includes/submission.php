8d2c80fc5e31a1e86af34c25adba0a90e5bf1921
jocampo <jomaro@globalteleforce.com>
2015-06-19 11:17:43 +0800

    Contact Form Error Check

--- wp-content/plugins/contact-form-7/includes/submission.php
+++ wp-content/plugins/contact-form-7/includes/submission.php
@@ -139,7 +139,7 @@ class WPCF7_Submission {
 				? $_POST['_wpcf7_unit_tag'] : '' );
 
 		$contact_form = $this->contact_form;
-print_r('<pre>');print_r($this->accepted());print_r('</pre>');exit;
+print_r('<pre>');print_r($this->spam());print_r('</pre>');exit;
 
 		if ( ! $this->validate() ) { // Validation error occured
 			$this->status = 'validation_failed';
