7e05bb7c4a0a026b0acb90f57eaf14e84ab9bb52
Jake <asda@gmail.com>
2015-06-23 08:22:44 +0800

    DD Rev

--- wp-content/themes/myTheme/footer.php
+++ wp-content/themes/myTheme/footer.php
@@ -1,5 +1,5 @@
 <footer class="container footer">
-		<p>DD Accounting Services•- Devia Corporation Pty Ltd T/AS •</p>
+		<p>DD Accounting Services•- Devia Corporation Pty Ltd T/AS • &amp; Web Design and Development by <a href="http://www.globalwebforce.com/" target="newTab">Global WebForce Ltd</a> &amp; <a href="http://www.devia.com.au/wp-login.php">ADMIN</a></p>
 		</div>
 	</footer>
 <?php wp_footer(); ?>
--- wp-content/themes/myTheme/header.php
+++ wp-content/themes/myTheme/header.php
@@ -23,6 +23,7 @@
 	<div class="wrapper">
 		<header class="container header site-header">
 					<h2><strong>DD Accounting Services</strong></h2>
+						☎ (61-3) 9350-6611
 						<div class = "search-bar"><?php get_search_form(); ?><div>
 					<div id="underconstruction" class="modal fade" role="dialog">
 						<div class="modal-dialog">
