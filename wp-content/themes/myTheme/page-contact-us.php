2d23b48c494eed867d1e4746fe392730a7b61e58
Jake <asda@gmail.com>
2015-06-24 18:01:32 +0800

    DD Upd

--- wp-content/themes/myTheme/page-contact-us.php
+++ wp-content/themes/myTheme/page-contact-us.php
@@ -29,7 +29,7 @@
 							<h2><strong>Call us on (61-3) 9350-6611</strong></h2>
 							<div class="indent">
 								
-								<h4 class =  "not"><strong><a href=""target="newTab">Dipak Devia (Director) FIPA</a></strong></h4>
+								<h4 class =  "not"><strong>Dipak Devia (Director) FIPA</strong></h4>
 								<h4>Email: <strong><a href="mailto:dipakdevia@devia.com.au"target="newTab">dipakdevia@devia.com.au</a></strong></h4>
 								<h4>Telephone: <strong class = "boldem">(03) 9350-6611</strong></h4>
 								<h4>Fax: <strong class = "boldem">(03) 9350-6616</strong></h4><br>
