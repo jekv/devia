1777b68c3023158acc91cee3b3ff9ed44c35895c
Jake <asda@gmail.com>
2015-06-24 16:48:48 +0800

    DD Upd

--- wp-content/themes/myTheme/page-contact-us-em.php
+++ wp-content/themes/myTheme/page-contact-us-em.php
@@ -1,6 +1,17 @@
-<?php get_header();
+<?php get_header(); ?>
 
-echo do_shortcode('[contact-form-7 id="205" title="Dipak"]');
+	<div class="container contactus-em">
+		<?php if ( have_posts() ) : ?>
+					 <?php while ( have_posts() ) : $i++;  the_post(); ?>
+							<p><?php the_content();?></p>
+						</div>
+					 
+					 <?php endwhile;
+					 else :
+					echo '<p> No content found </p>';
+				  	endif; ?>
 
-get_footer();
-?>
+					 ?>
+
+	</div>
+<?php get_footer();?>
