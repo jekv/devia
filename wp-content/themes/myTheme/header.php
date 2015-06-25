6ec8b5dc632f528dbc7678dde3ecd95dadeb1ae4
Jake <asda@gmail.com>
2015-06-25 08:13:16 +0800

    DD Upd

--- wp-content/themes/myTheme/header.php
+++ wp-content/themes/myTheme/header.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 	<!--Bootstrap-->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 	<!-- Optional theme -->
