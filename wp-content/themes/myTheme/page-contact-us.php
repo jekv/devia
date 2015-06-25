9bff1f44b67ddbd02e6487c9e4c64c1156e24356
Jake <asda@gmail.com>
2015-06-25 10:15:14 +0800

    DD Upd

--- wp-content/themes/myTheme/page-contact-us.php
+++ wp-content/themes/myTheme/page-contact-us.php
@@ -8,7 +8,7 @@
 			<div class = "col-md-6">
 					<img src="<?php the_post_thumbnail(); ?>" alt="" class="img-responsive">
 					<h2><?php the_title(); ?></h2>
-					<h4>It's really easy to get in touch with us! Please fill in the form below or email us at <b>dipakdevia@devia.com.au</b> or minadevia@devia.com.au and we'll get back to you as soon as possible. Or if you want to speak to our friendly staff right now then please call <b>(61-3) 9350-6611</b>.</h4>
+					<h4>It's really easy to get in touch with us! Please fill in the form below or email us at <b><a href="mailto:minadevia@devia.com.au">dipakdevia@devia.com.au</a></b> or <b><a href="mailto:minadevia@devia.com.au">minadevia@devia.com.au</a></b> and we'll get back to you as soon as possible. Or if you want to speak to our friendly staff right now then please call <b>(61-3) 9350-6611</b>.</h4>
 					<div class="wrapper ff">
 						<div class="fillup-form">
 							<p><?php the_content();?></p>
