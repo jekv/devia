7d2556eaa5234ba18acb3cb47ad912dfa865566f
Jake <asda@gmail.com>
2015-06-25 08:11:27 +0800

    DD Upd

--- wp-content/themes/myTheme/footer.php
+++ wp-content/themes/myTheme/footer.php
@@ -1,7 +1,7 @@
-<footer class="container footer">
+<div class="container footer">
 		<p>DD Accounting Services•- Devia Corporation Pty Ltd T/AS • &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Web Design and Development by <a href="http://www.globalwebforce.com/" target="newTab">Global WebForce Ltd</a> &nbsp;&nbsp;&nbsp; — &nbsp;&nbsp;&nbsp;<a href="http://www.devia.com.au/wp-login.php">ADMIN</a></p>
 		</div>
-	</footer>
+	</div>
 <?php wp_footer(); ?>
 
 </body>
