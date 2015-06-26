b76315481c8bf9310b7c4ba760705da29e5bbd1d
Jake <asda@gmail.com>
2015-06-26 13:41:18 +0800

    DD Upd

--- wp-content/themes/myTheme/page.php
+++ wp-content/themes/myTheme/page.php
@@ -31,7 +31,7 @@
 
 			<div class = "midContent">
 			
-				<h1>Our Services</h1>
+				<h1>OUR SERVICES</h1>
 		<?php query_posts('category_name='.get_the_title().'&post_status=publish,future');?>
 			
 			<div class = "row">
