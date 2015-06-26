8e331c2d1f13200ae86a045ee1b0cfad4af31639
Jake <asda@gmail.com>
2015-06-26 11:27:32 +0800

    DD UPd

--- wp-content/themes/myTheme/page-partners.php
+++ wp-content/themes/myTheme/page-partners.php
@@ -1,15 +1,15 @@
 <?php get_header() ?>
 
-	<?php if(have_posts()) : while (have_posts()) : the_post(); ?>
 
 	<div class="container mainContent-partners">
 		<div class="row">
 			<div class="col-md-8 out8">
 				<h5>You are here. <a href="index.html">Home</a> <i class="fa fa-angle-double-right"></i> Partners</h5>
  				<hr class = "fhr">
+				<?php if(have_posts()) : while (have_posts()) : the_post(); ?>
 				<h2><?php the_title(); ?></h2>
 				<?php the_content(); ?>
-			
+					<?php endwhile; endif; ?>
 			</div>
 			<div class="col-md-4 in4 out4">
 				<div class="reg">
@@ -38,5 +38,4 @@
 			</div>
 		</div>
 	</div>
-	<?php endwhile; endif; ?>
 <?php get_footer() ?>
